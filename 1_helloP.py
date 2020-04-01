d45480e1c9b413ef6f4087d67d187c3a13f3298c
Ravi Tambade <ravi.tambade@transflower.in>
2020-04-01 11:30:02 +0530

    Simple python file
    sssss

---
+++ 1_helloP.py
@@ -0,0 +1,293 @@
+https://www.onlinegdb.com/online_python_interpreter
+
+https://docs.python.org/3/library/multiprocessing.html
+
+
+https://medium.com/idealo-tech-blog/parallelisation-in-python-an-alternative-approach-b2749b49a1e
+
+
+
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3]))
+
+
+
+
+
+
+
+
+from multiprocessing import Process
+
+def f(name):
+    print('hello', name)
+
+if __name__ == '__main__':
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
+
+
+
+
+
+
+
+from multiprocessing import Process
+import os
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+def f(name):
+    info('function f')
+    print('hello', name)
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
+
+
+
+
+
+    import multiprocessing as mp
+
+def foo(q):
+    q.put('hello')
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
+
+
+
+
+    import multiprocessing as mp
+
+def foo(q):
+    q.put('hello')
+
+if __name__ == '__main__':
+    ctx = mp.get_context('spawn')
+    q = ctx.Queue()
+    p = ctx.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
+
+
+
+
+    from multiprocessing import Process, Queue
+
+def f(q):
+    q.put([42, None, 'hello'])
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    print(q.get())    # prints "[42, None, 'hello']"
+    p.join()
+
+
+
+
+
+
+
+
+
+from multiprocessing import Process, Lock
+
+def f(l, i):
+    l.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        l.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+
+    for num in range(10):
+        Process(target=f, args=(lock, num)).start()
+
+
+
+
+
+
+
+
+from multiprocessing import Process, Lock
+from multiprocessing.sharedctypes import Value, Array
+from ctypes import Structure, c_double
+
+class Point(Structure):
+    _fields_ = [('x', c_double), ('y', c_double)]
+
+def modify(n, x, s, A):
+    n.value **= 2
+    x.value **= 2
+    s.value = s.value.upper()
+    for a in A:
+        a.x **= 2
+        a.y **= 2
+
+if __name__ == '__main__':
+    lock = Lock()
+
+    n = Value('i', 7)
+    x = Value(c_double, 1.0/3.0, lock=False)
+    s = Array('c', b'hello world', lock=lock)
+    A = Array(Point, [(1.875,-6.25), (-5.75,2.0), (2.375,9.5)], lock=lock)
+
+    p = Process(target=modify, args=(n, x, s, A))
+    p.start()
+    p.join()
+
+    print(n.value)
+    print(x.value)
+    print(s.value)
+    print([(a.x, a.y) for a in A])
+
+
+
+
+
+
+
+
+"""
+Python Parallel Processing (in 60 seconds or less)
+https://dbader.org/blog/python-parallel-computing-in-60-seconds
+"""
+import collections
+import multiprocessing
+
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'born',
+])
+
+scientists = (
+    Scientist(name='Ada Lovelace', born=1815),
+    Scientist(name='Emmy Noether', born=1882),
+    Scientist(name='Marie Curie', born=1867),
+    Scientist(name='Tu Youyou', born=1930),
+    Scientist(name='Ada Yonath', born=1939),
+    Scientist(name='Vera Rubin', born=1928),
+    Scientist(name='Sally Ride', born=1951),
+)
+
+def process_item(item):
+    return {
+        'name': item.name,
+        'age': 2017 - item.born
+    }
+
+pool = multiprocessing.Pool()
+result = pool.map(process_item, scientists)
+
+print(tuple(result))
+
+
+
+
+
+
+
+
+#!/usr/bin/env python3
+
+import multiprocessing
+import threading
+import time
+import sys
+
+def cpu_func(result, niters):
+    '''
+    A useless CPU bound function.
+    '''
+    for i in range(niters):
+        result = (result * result * i + 2 * result * i * i + 3) % 10000000
+    return result
+
+class CpuThread(threading.Thread):
+    def __init__(self, niters):
+        super().__init__()
+        self.niters = niters
+        self.result = 1
+    def run(self):
+        self.result = cpu_func(self.result, self.niters)
+
+class CpuProcess(multiprocessing.Process):
+    def __init__(self, niters):
+        super().__init__()
+        self.niters = niters
+        self.result = 1
+    def run(self):
+        self.result = cpu_func(self.result, self.niters)
+
+class IoThread(threading.Thread):
+    def __init__(self, sleep):
+        super().__init__()
+        self.sleep = sleep
+        self.result = self.sleep
+    def run(self):
+        time.sleep(self.sleep)
+
+class IoProcess(multiprocessing.Process):
+    def __init__(self, sleep):
+        super().__init__()
+        self.sleep = sleep
+        self.result = self.sleep
+    def run(self):
+        time.sleep(self.sleep)
+
+if __name__ == '__main__':
+    cpu_n_iters = int(sys.argv[1])
+    sleep = 1
+    cpu_count = multiprocessing.cpu_count()
+    input_params = [
+        (CpuThread, cpu_n_iters),
+        (CpuProcess, cpu_n_iters),
+        (IoThread, sleep),
+        (IoProcess, sleep),
+    ]
+    header = ['nthreads']
+    for thread_class, _ in input_params:
+        header.append(thread_class.__name__)
+    print(' '.join(header))
+    for nthreads in range(1, 2 * cpu_count):
+        results = [nthreads]
+        for thread_class, work_size in input_params:
+            start_time = time.time()
+            threads = []
+            for i in range(nthreads):
+                thread = thread_class(work_size)
+                threads.append(thread)
+                thread.start()
+            for i, thread in enumerate(threads):
+                thread.join()
+            results.append(time.time() - start_time)
+        print(' '.join('{:.6e}'.format(result) for result in results))
